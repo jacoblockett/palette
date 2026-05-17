9693abb4e24df891711bf129b87e8e68434229df
Jacob Lockett <jacobwyattlockett@gmail.com>
2026-05-17 18:11:08 -0400

    feat: add hex and RGB color primitives

--- src/color/normalize.js
+++ src/color/normalize.js
@@ -1,3 +1,20 @@
-export function normalizeColor() {
-	throw new Error("normalizeColor has not been implemented")
+const SHORT_HEX_PATTERN = /^#[\da-fA-F]{3}$/
+const LONG_HEX_PATTERN = /^#[\da-fA-F]{6}$/
+
+export function normalizeHex(value) {
+	if (typeof value !== "string") {
+		throw new TypeError("Expected a hex color string")
+	}
+
+	if (SHORT_HEX_PATTERN.test(value)) {
+		const [, r, g, b] = value.toLowerCase()
+
+		return `#${r}${r}${g}${g}${b}${b}`
+	}
+
+	if (LONG_HEX_PATTERN.test(value)) {
+		return value.toLowerCase()
+	}
+
+	throw new TypeError("Expected a hex color in #rgb or #rrggbb format")
 }
