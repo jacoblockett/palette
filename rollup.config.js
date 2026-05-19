40f1faa43bce6578c09661fa13facc6d44eabc12
Jacob Lockett <jacobwyattlockett@gmail.com>
2026-05-19 14:26:45 -0400

    build: generate package output with rollup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import commonjs from "@rollup/plugin-commonjs"
+import resolve from "@rollup/plugin-node-resolve"
+
+export default [
+	{
+		input: "src/index.js",
+		output: [
+			{ file: "dist/index.cjs", format: "cjs" },
+			{ file: "dist/index.mjs", format: "es" }
+		],
+		plugins: [resolve(), commonjs()]
+	}
+]
