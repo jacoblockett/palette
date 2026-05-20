bf501e7e041aaf121d797c483bee354bc6255f59
Jacob Lockett <jacobwyattlockett@gmail.com>
2026-05-20 13:09:55 -0400

    feat(demo): convert landing demo to svelte

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,48 @@
 import commonjs from "@rollup/plugin-commonjs"
 import resolve from "@rollup/plugin-node-resolve"
+import terser from "@rollup/plugin-terser"
+import copy from "rollup-plugin-copy"
+import css from "rollup-plugin-css-only"
+import svelte from "rollup-plugin-svelte"
+
+const production = process.env.NODE_ENV === "production"
 
 export default [
 	{
 		input: "src/index.js",
 		output: [
-			{ file: "dist/index.cjs", format: "cjs" },
-			{ file: "dist/index.mjs", format: "es" }
+			{ file: "dist/index.cjs", format: "cjs", sourcemap: !production },
+			{ file: "dist/index.mjs", format: "es", sourcemap: !production }
 		],
 		plugins: [resolve(), commonjs()]
+	},
+	{
+		input: "demo/src/main.js",
+		output: {
+			file: "demo/dist/bundle.js",
+			format: "iife",
+			name: "paletteDemo",
+			sourcemap: !production
+		},
+		plugins: [
+			svelte({
+				compilerOptions: {
+					dev: !production
+				},
+				emitCss: true
+			}),
+			css({ output: "demo/dist/bundle.css" }),
+			resolve({
+				browser: true,
+				exportConditions: ["svelte"],
+				extensions: [".mjs", ".js", ".svelte"],
+				dedupe: ["svelte"]
+			}),
+			commonjs(),
+			production && terser(),
+			copy({
+				targets: [{ src: "demo/public/index.html", dest: "demo/dist" }]
+			})
+		].filter(Boolean)
 	}
 ]
